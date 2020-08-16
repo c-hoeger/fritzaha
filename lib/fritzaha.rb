de1daccae8341f17719672cbe4dfa85c28572e2e
c-hoeger <github@choeger.de>
2020-08-16 12:52:59 +0200

    - debug mode - debug output on stderr - exit login in case of wrong login/password

--- lib/fritzaha.rb
+++ lib/fritzaha.rb
@@ -13,15 +13,20 @@ class FritzAHA
     @fburl    = fburl
     @sidfile  = sidfile
     @sid      = nil
+    @debug    = false
   end
 
   def login
     if File.exists?(@sidfile)
       @sid = File.open(@sidfile,'r').gets.chomp
-      #puts "old sid=#{sid}"
+      STDERR.puts "old sid=#{sid}" if @debug
     else
       _login
     end
+  end
+
+  def debug(debug)
+    @debug = debug
   end
 
   def sid
@@ -38,6 +43,11 @@ class FritzAHA
 
   private
 
+  def _debug_response(func, response)
+    STDERR.puts "#{func} - #{response.code}" if @debug
+    STDERR.puts "#{func} - #{response.msg}" if @debug
+  end
+
   def _cmd(cmd,ain)
     if nil == @sid
        raise "no session, forgot to execute login?"
@@ -49,10 +59,9 @@ class FritzAHA
 
     Net::HTTP.start(uri.host, uri.port, :use_ssl => true, :verify_mode => OpenSSL::SSL::VERIFY_NONE) do |http|
       request = Net::HTTP::Get.new uri
-      response = http.request request # Net::HTTPResponse object
+      response = http.request request
       if response.code != "200"
-         #puts response.code
-         #puts response.msg
+         _debug_response('_cmd', response)
          if response.code == "403"
            _login
            return _cmd(cmd, ain)
@@ -69,34 +78,35 @@ class FritzAHA
     @sid = challenge = nil
     Net::HTTP.start(uri.host, uri.port, :use_ssl => true, :verify_mode => OpenSSL::SSL::VERIFY_NONE) do |http|
       request = Net::HTTP::Get.new uri
-      response = http.request request # Net::HTTPResponse object
-      #p response.body
-      doc = Document.new(response.body)
-      @sid=XPath.first(doc, '//SID').text
+      response = http.request request
+      _debug_response('_login(init)', response)
+      doc  = Document.new(response.body)
+      @sid = XPath.first(doc, '//SID').text
       challenge=XPath.first(doc, '//Challenge').text
     end
 
-    #puts "got sid=#{sid}, challenge=#{challenge}"
+    STDERR.puts "got sid=#{sid}, challenge=#{challenge}" if @debug
 
-    if @sid == "0000000000000000"
+    if @sid == '0000000000000000'
       # create new sid only in case it has the value above
       md5=Digest::MD5.hexdigest("#{challenge}-#{@fbpasswd}".encode(Encoding::UTF_16LE))
       response="#{challenge}-#{md5}"
 
-      #puts "response=#{response}"
+      STDERR.puts "response=#{response}" if @debug
 
       Net::HTTP.start(uri.host, uri.port, :use_ssl => true, :verify_mode => OpenSSL::SSL::VERIFY_NONE) do |http|
         request = Net::HTTP::Post.new uri
         request.set_form_data('response' => response, 'page' => '', 'username' => @fblogin)
-        response = http.request request # Net::HTTPResponse object
-        #p response.body
-        doc = Document.new(response.body)
-        @sid=XPath.first(doc, '//SID').text
+        response = http.request request
+        _debug_response('_login(getsid)', response)
+        doc  = Document.new(response.body)
+        @sid = XPath.first(doc, '//SID').text
+        raise "unable to get a valid session; wrong login and/or password?" if @sid == '0000000000000000'
       end
 
+      STDERR.puts "saving new sid=#{sid}" if @debug
       File.open(@sidfile,'w').puts(@sid)
 
-      #puts "saving new sid=#{sid}"
     end
   end
 end
